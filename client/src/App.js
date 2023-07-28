3bc08f4211a26584aa5a41e74ec8db748ece34d6
Saurabh Jain <jsaurabh667@gmail.com>
2023-07-28 19:36:54 +0530

    Register page is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {BrowserRouter as Router,
+Routes, Route} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 export default function App() {
   return (
-    <div>
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/register"  element={<Register/>}/>
+        <Route exact path="/login"  element={<Login/>}/>
+        <Route exact path="/"  element={<Chat/>}/>
+      </Routes>
+    </Router>
   )
 }
